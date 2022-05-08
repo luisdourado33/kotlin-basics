03de72f8b5c7a1c85498be6f9b9b3604e5ca87d6
Luis Dourado <luis_dourado33@hotmail.com>
2022-05-08 16:16:19 -0300

    chore(*): first commit

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     lateinit var userEmail: EditText
@@ -13,5 +15,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        userEmail = findViewById(R.id.userEmail)
+        userPassword = findViewById(R.id.userPassword)
+        submitButton = findViewById(R.id.loginSubmit)
+
+        submitButton.setOnClickListener(object: View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(applicationContext, "My first message!", Toast.LENGTH_LONG).show()
+            }
+        })
     }
 }
